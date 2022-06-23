10e9a9a6fd46814a60487d077cd6eca402396b53
Guillaume ARM <trapcodien@gmail.com>
2022-06-23 13:06:56 +0200

    feat: add seeAmmotItemColors function

--- src/mod.ts
+++ src/mod.ts
@@ -1,10 +1,44 @@
 import { DependencyContainer } from "tsyringe";
+
 import { IMod } from "@spt-aki/models/external/mod";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+
 import { getModDisplayName } from "./utils";
 
 class Mod implements IMod {
   private logger: ILogger;
+
+  private setAmmoItemColors(container: DependencyContainer) {
+    const database = container.resolve<DatabaseServer>("DatabaseServer");
+    const tables = database.getTables();
+
+    const items = tables.templates.items;
+
+    for (const i in items) {
+      const item = items[i]
+
+      //set baground colour of ammo depending on pen
+      if (item._parent === "5485a8684bdc2da71d8b4567") {
+        const pen = item._props.PenetrationPower
+        let color = "grey"
+
+        if (pen > 60) {
+          color = "red";
+        } else if (pen > 50) {
+          color = "yellow";
+        } else if (pen > 40) {
+          color = 'violet';
+        } else if (pen > 30) {
+          color = 'blue';
+        } else if (pen > 20) {
+          color = 'green'
+        }
+
+        item._props.BackgroundColor = color
+      }
+    }
+  }
 
   public load(container: DependencyContainer): void {
     this.logger = container.resolve<ILogger>("WinstonLogger");
@@ -12,7 +46,7 @@ class Mod implements IMod {
   }
 
   public delayedLoad(container: DependencyContainer): void {
-    void container;
+    this.setAmmoItemColors(container);
     this.logger.success(`=> Successfully loaded ${getModDisplayName(true)}`);
   }
 }
